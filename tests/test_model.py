532ca043d1073d32fcfb6da3d4f8b3ca16aee4b4
Joshua Ivanhoe <joshua@h2gopower.com>
2024-01-12 11:09:12 +0000

    Added infeasible test cases to test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,19 +4,23 @@ import pytest
 from halfspace import Model
 from halfspace.model import Var
 
+from typing import Optional
+import numpy as np
 VAR_TOL: float = 1e-2
 
 
 def _check_solution(
     model: Model,
-    expected_objective_value: float,
-    expected_solution: dict[Var, float],
+    expected_objective_value: Optional[float],
+    expected_solution: Optional[dict[Var, float]],
     expected_status: mip.OptimizationStatus = mip.OptimizationStatus.OPTIMAL,
 ):
+    if expected_objective_value is not None:
+        assert model.objective_value == pytest.approx(expected_objective_value, abs=model.max_gap_abs)
+    if expected_solution is not None:
+        for x, expected_value in expected_solution.items():
+            assert model.var_value(x=x) == pytest.approx(expected_value, abs=VAR_TOL)
     assert model.status == expected_status
-    assert model.objective_value == pytest.approx(expected_objective_value, abs=model.max_gap_abs)
-    for x, expected_value in expected_solution.items():
-        assert model.var_value(x=x) == pytest.approx(expected_value, abs=VAR_TOL)
 
 
 def test_single_variable_no_constraints():
@@ -94,6 +98,21 @@ def test_multivariable_linear_constraint():
     )
 
 
+def test_multivariable_linear_constraint_infeasible():
+    model = Model()
+    x = model.add_var(lb=0, ub=1)
+    y = model.add_var(lb=0, ub=1)
+    model.add_objective_term(var=(x, y), func=lambda x, y: (x - 0.25) ** 2 + (y - 0.25) ** 2 + 1)
+    model.add_linear_constr(x + y >= 3)
+    model.optimize()
+    _check_solution(
+        model=model,
+        expected_objective_value=None,
+        expected_solution=None,
+        expected_status=mip.OptimizationStatus.INFEASIBLE,
+    )
+
+
 def test_multivariable_nonlinear_constraint():
     model = Model(max_gap_abs=1e-2)
     x = model.add_var(lb=0, ub=1)
@@ -106,3 +125,21 @@ def test_multivariable_nonlinear_constraint():
         expected_objective_value=1 + 0.25 ** 2,
         expected_solution={x: 0., y: 0.25},
     )
+
+
+def test_multivariable_nonlinear_constraint_infeasible():
+    model = Model(max_gap_abs=1e-2)
+    x = model.add_var(lb=0, ub=1)
+    y = model.add_var(lb=0, ub=1)
+    model.add_objective_term(var=(x, y), func=lambda x, y: (x - 0.25) ** 2 + (y - 0.25) ** 2 + 1)
+    model.add_nonlinear_constr(var=(x, y), func=lambda x, y: np.exp(x + y) + 1)
+    model.optimize()
+    _check_solution(
+        model=model,
+        expected_objective_value=None,
+        expected_solution=None,
+        expected_status=mip.OptimizationStatus.INFEASIBLE,
+    )
+
+
+
